79b74f65a61490515cb57c32644cb2473dc682c3
BenteO <ottben14@student.westerdals.no>
2015-03-08 18:05:29 +0100

    Lagt til menu scene

---
+++ Assets/Script/BallNew.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallNew : MonoBehaviour {
+
+	public float ballInitialVelocity = 600f;
+
+	private Rigidbody rb;
+	private bool ballInPlay;
+
+	// Use this for initialization
+	void Awake () {
+	
+        // hämtar komponenten rigidbody som vi ska jobba med
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown ("Fire1") && ballInPlay == false) {
+			// Ball inte längre parent av paddle
+            transform.parent = null;
+			ballInPlay = true;
+			// slår av kinematic
+            rb.isKinematic = false;
+			// Kraften som bollen slungas iväg med
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+		} 
+	}
+}
